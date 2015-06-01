679119f855a3b7c3a7ec8f2bab48abb946aa57c5
Joshua Marquez N <josuehenry14@hotmail.com>
2015-06-01 11:43:29 -0700

    Initial commit

---
+++ scripts/scripts.8547cf83.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("josuehenrygithubioApp",["ngAnimate","ngMessages","ngSanitize","ngTouch"]),angular.module("josuehenrygithubioApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
